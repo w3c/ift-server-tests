785d2bc3f75e00ad1a1fc320a6b701717be63245
Garret Rieger <grieger@google.com>
2022-03-31 14:54:46 -0700

    Add function to load axis space from font.

--- font_util.py
+++ font_util.py
@@ -1,5 +1,6 @@
 """Utility for geting info from fonts."""
 
+import bisect
 import io
 
 from fontTools.ttLib import TTFont
@@ -21,5 +22,57 @@ def codepoints(font_data):
 
 
 def axis_space(font_data):
-  # TODO(garretrieger)
-  pass
+  font = TTFont(io.BytesIO(font_data))
+  if "fvar" not in font:
+    return {}
+
+  fvar = font["fvar"]
+  space = {}
+  for axis in fvar.axes:
+    tag = axis.axisTag
+    if tag not in space:
+      space[tag] = []
+
+    if axis.minValue > axis.maxValue:
+      raise Exception(f"Invalid font min value ({axis.minValue} > max value ({axis.maxValue} for "
+                      f"{tag} axis.")
+
+    add_intervals(space[tag], {0: axis.minValue, 1: axis.maxValue})
+
+  return space
+
+
+def add_intervals(intervals, interval):
+  """Union interval into intervals which is sorted by start value."""
+  bisect.insort(intervals, interval, key=lambda interval: interval[0])
+  merged = []
+  i = 0
+  while i < len(intervals):
+    current = intervals[i]
+
+    while i < len(intervals) - 1 and intersects(current, intervals[i+1]):
+      current = union(current, intervals[i+1])
+      i += 1
+
+    merged.append(current)
+    i += 1
+
+  intervals[:] = merged
+
+
+def union(interval_1, interval_2):
+  a_start = interval_1[0]
+  b_start = interval_2[0]
+  a_end = interval_1[1]
+  b_end = interval_2[1]
+
+  return {0: min(a_start, b_start), 1: max(a_end, b_end)}
+
+
+def intersects(interval_1, interval_2):
+  a_start = interval_1[0]
+  b_start = interval_2[0]
+  a_end = interval_1[1]
+  b_end = interval_2[1]
+
+  return b_start <= a_end and a_start <= b_end
